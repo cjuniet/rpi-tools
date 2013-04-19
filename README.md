2ef0bad2f3e8666caef2e2e6cb513899e723dcbd
Christophe Juniet <c.juniet@gmail.com>
2013-04-20 00:35:55 +0200

    Add tmux-stats

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 rpi-tools
 =========
 
-Raspberry Pi Tools
+A few utilities I've made on my Raspberry Pi.
+
+tmux-stats
+----------
+Prints a rather minimalist list with the load-average, the cpu usage and the memory usage.
+I use it in my tmux status bar.
+
+Example:
+
+	$ echo `tmux-stats`
+	L:0.27 C:2% M:93%
+
---
+++ tmux-stats.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+//  LAVG CPU% MEM%
+// "9.99 100% 100%"
+
+#define BUFSIZE 64
+
+int g_delay = 1000000;
+
+size_t get_load(char *buf, size_t len)
+{
+  char line[BUFSIZE];
+  size_t n = 0;
+  FILE *fp = fopen("/proc/loadavg", "r");
+  if (fp == NULL) exit(EXIT_FAILURE);
+
+  fgets(line, BUFSIZE, fp);
+  n = snprintf(buf, len, "L:%s ", strtok(line, " "));
+
+  fclose(fp);
+
+  return n;
+}
+
+void get_cpu_usage(int *used, int *unused)
+{
+  char line[BUFSIZE];
+  int cpu_user, cpu_nice, cpu_sys, cpu_idle;
+  FILE *fp = fopen("/proc/stat", "r");
+  if (fp == NULL) exit(EXIT_FAILURE);
+
+  fgets(line, BUFSIZE, fp);
+  strtok(line, " "); // discard 'cpu'
+  cpu_user = atoi(strtok(NULL, " "));
+  cpu_nice = atoi(strtok(NULL, " "));
+  cpu_sys  = atoi(strtok(NULL, " "));
+  cpu_idle = atoi(strtok(NULL, " "));
+
+  *used = cpu_user + cpu_nice + cpu_sys;
+  *unused = cpu_idle;
+
+  fclose(fp);
+}
+
+size_t get_cpu(char *buf, size_t len)
+{
+  size_t n = 0;
+  int usedA, unusedA, usedB, unusedB;
+  int used, total;
+
+  get_cpu_usage(&usedA, &unusedA);
+
+  usleep(g_delay);
+
+  get_cpu_usage(&usedB, &unusedB);
+
+  used = usedB - usedA;
+  total = used + (unusedB - unusedA);
+  n = snprintf(buf, len, "C:%d%% ", (int)(0.5 + 100.0 * used / total));
+
+  return n;
+}
+
+int get_mem_next_value(FILE *fp)
+{
+  char line[BUFSIZE];
+  fgets(line, BUFSIZE, fp);
+  strtok(line, " "); // discard name
+  return atoi(strtok(NULL, " "));
+}
+
+size_t get_mem(char *buf, size_t len)
+{
+  size_t n = 0;
+  int total, used;
+  FILE *fp = fopen("/proc/meminfo", "r");
+  if (fp == NULL) exit(EXIT_FAILURE);
+
+  // get the total and free memory values
+  total = get_mem_next_value(fp);
+  used = total - get_mem_next_value(fp);
+
+  n = snprintf(buf, len, "M:%d%% ", (int)(0.5 + 100.0 * used / total));
+
+  fclose(fp);
+
+  return n;
+}
+
+int main(int argc, char *argv[])
+{
+  char buf[BUFSIZE];
+  size_t len = 0;
+
+  if (argc > 1) {
+    g_delay = atoi(argv[1]) * 1000000;
+    if (g_delay < 1000000) g_delay = 1000000;
+    if (g_delay > 10000000) g_delay = 10000000;
+  }
+
+  len += get_load(buf + len, BUFSIZE - len);
+
+  len += get_cpu(buf + len, BUFSIZE - len);
+
+  len += get_mem(buf + len, BUFSIZE - len);
+
+  buf[len-1] = '\0'; // eat last space
+  printf("%s", buf);
+
+  return 0;
+}
